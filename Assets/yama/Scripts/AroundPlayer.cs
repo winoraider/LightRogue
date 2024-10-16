2c9b1e4cb9d3ecffa852b8fdc5fa1e2e2858fac3
Yuki <sdt22csc0018@edu.sca.ac.jp>
2024-10-16 14:53:25 +0900

    もの(Efect)がobjectの周りをまわるようにした

---
+++ Assets/yama/Scripts/AroundPlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AroundPlayer : MonoBehaviour
+{
+    [SerializeField] float angle = 0.0f;
+    [SerializeField] float radius = 2.0f;
+    [SerializeField] GameObject center = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = RotateAroundZ(center.transform.position, angle, radius);
+        angle += 0.05f;
+    }
+
+    // ZŽ²‰ñ“]
+    static Vector3 RotateAroundZ(Vector3 original_position, float angle, float radius)
+    {
+        Vector3 v = original_position;
+        v.y += radius;
+        float a = angle * Mathf.Deg2Rad;
+        float x = Mathf.Cos(a) * v.x - Mathf.Sin(a) * v.y;
+        float y = Mathf.Sin(a) * v.x + Mathf.Cos(a) * v.y;
+        float z = v.z;
+        return new Vector3(x, y, z);
+    }
+}
