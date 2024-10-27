08edd1e67a55b1eabd73e463c6bceb3cfb267eed
yama <sdt2395gp0020@edu.sca.ac.jp>
2024-10-27 15:56:10 +0900

    色によって弾周りのくるくる出る

--- Assets/yama/Efects/AroundPlayer.cs
+++ Assets/yama/Efects/AroundPlayer.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class AroundPlayer : MonoBehaviour
 {
+    [SerializeField] Transform around;
     [SerializeField] float angleSpeed;
     [SerializeField] float angle = 0.0f;
     [SerializeField] float radius = 2.0f;
     [SerializeField] GameObject center;
+
+    private bool isActive = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +20,10 @@ public class AroundPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!isActive) return;
         //transform.position = center.transform.position;
-        transform.position = RotateAroundZ(center.transform.position, angle, radius);
-        angle += angleSpeed;
+        around.position = RotateAroundZ(center.transform.position, angle, radius);
+        angle += angleSpeed * Time.deltaTime;
     }
 
     // ZŽ²‰ñ“]
@@ -32,5 +36,16 @@ public class AroundPlayer : MonoBehaviour
         float y = Mathf.Sin(a) * v.x + Mathf.Cos(a) * v.y;
         float z = v.z;
         return new Vector3(x, y, z);
+    }
+
+    public void SetAngle(float angle)
+    {
+        this.angle = angle;
+    }
+
+    public void Active()
+    {
+        around.gameObject.SetActive(true);
+        isActive = true;
     }
 }
---
+++ Assets/yama/Efects/AroundPlayerManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AroundPlayerManager : MonoBehaviour
+{
+    [SerializeField] private AroundPlayer redAround;
+    [SerializeField] private AroundPlayer greenAround;
+    [SerializeField] private AroundPlayer blueAround;
+
+    private bool isRed = false;
+    private bool isGreen = false;
+    private bool isBlue = false;
+
+    GameManager gameManager;
+
+    private void Update()
+    {
+        /*if (gameManager.isRed)
+        {
+            RedActivate();
+        }
+        if (gameManager.isGreen)
+        {
+            GreenActivate();
+        }
+        if (gameManager.isBlue)
+        {
+            BlueActivate();
+        }*/
+        if (Input.GetKeyDown(KeyCode.I)) RedActivate();
+        if (Input.GetKeyDown(KeyCode.O)) BlueActivate(); 
+        if (Input.GetKeyDown(KeyCode.P)) GreenActivate(); 
+    }
+
+    public void RedActivate()
+    {
+        redAround.Active();
+        isRed = true;
+        CheckCount();
+    }
+    public void GreenActivate()
+    {
+        greenAround.Active();
+        isGreen = true;
+        CheckCount();
+    }
+    public void BlueActivate()
+    {
+        blueAround.Active();    
+        isBlue = true;
+        CheckCount();
+    }
+
+    private void CheckCount()
+    {
+        int count = 0;
+        List<AroundPlayer> aroundPlayers = new List<AroundPlayer>();
+        if (isRed)
+        {
+            count++;
+            aroundPlayers.Add(redAround);
+        }
+        if(isGreen)
+        {
+            count++;
+            aroundPlayers.Add(greenAround);
+        }
+        if (isBlue)
+        {
+            count++;
+            aroundPlayers.Add(blueAround);
+        }
+        for(int i = 0; i < aroundPlayers.Count; i++)
+        {
+            aroundPlayers[i].SetAngle(360f / count * i);
+        }
+    }
+}
