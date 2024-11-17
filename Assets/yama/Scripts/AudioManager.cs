d3de008966fcb259843b246594a1d9ac50dfd8d5
kanta <>
2024-11-17 16:22:44 +0900

    エフェクトくれたたけちーが

--- Assets/yama/Scripts/AudioManager.cs
+++ Assets/yama/Scripts/AudioManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Timeline;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class AudioManager : MonoBehaviour
 {
