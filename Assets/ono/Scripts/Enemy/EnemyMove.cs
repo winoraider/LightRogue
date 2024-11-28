8ca74beb3f1230b298667d9714cf4ae10f36b6e8
Cicle714 <>
2024-11-28 11:14:17 +0900

    経験値にエフェクトを付けた

--- Assets/ono/Scripts/Enemy/EnemyMove.cs
+++ Assets/ono/Scripts/Enemy/EnemyMove.cs
@@ -51,4 +51,8 @@ public class EnemyMove : MonoBehaviour
             action = false;
         }
     }
+    void OnDestroy()
+    {
+        Instantiate(EXPPoint, transform.position, Quaternion.identity);Å@//îˆå`Ç¢Ç∂Ç¡ÇΩ 
+    }
 }
